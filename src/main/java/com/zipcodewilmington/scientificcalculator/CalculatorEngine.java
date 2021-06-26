e27ccb715c2c017c18e13e5bae0d3bdf45264b74
Nathan <nathan@zipcodes-MacBook-Pro-9.local>
2021-06-26 17:49:53 -0400

    Fleshed out CalculatorEngine

---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/CalculatorEngine.java
@@ -0,0 +1,69 @@
+package com.zipcodewilmington.scientificcalculator;
+
+public class CalculatorEngine {
+
+    public static Double add(Double input1, Double input2) {
+        return null;
+    }
+
+    public static Double subtract(Double input1, Double input2) {
+        return null;
+    }
+
+    public static Double divide(Double input1, Double input2) {
+        return null;
+    }
+
+    public static Double multiply(Double input1, Double input2) {
+        return null;
+    }
+
+    public static Double exponent(Double input1, Double input2) {
+        return null;
+    }
+
+    public static Double squareRoot(Double input1, Double input2) {
+        return null;
+    }
+
+    public static Double square(Double input1, Double input2) {
+        return null;
+    }
+
+    public static Double inverse(Double input1, Double input2) {
+        return null;
+    }
+
+    public static Double switchSign(Double input1, Double input2) {
+        return null;
+    }
+
+    public static Double sine(Double input1, Double input2) {
+        return null;
+    }
+
+    public static Double cosine(Double input1, Double input2) {
+        return null;
+    }
+
+    public static Double tangent(Double input1, Double input2) {
+        return null;
+    }
+
+    public static Double inverseSine(Double input1, Double input2) {
+        return null;
+    }
+
+    public static Double inverseCosine(Double input1, Double input2) {
+        return null;
+    }
+
+    public static Double inverseTangent(Double input1, Double input2) {
+        return null;
+    }
+
+    public static Double factorial(Double input1, Double input2) {
+        return null;
+    }
+
+}
